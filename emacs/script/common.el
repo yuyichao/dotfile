c9cff9e3dab84a9c3623d3cba6c21fdd964c8a0a
Yichao Yu <yyc1992@gmail.com>
2012-07-05 12:31:42 -0400

    [emacs] automatically make new directory

--- emacs/script/common.el
+++ emacs/script/common.el
@@ -2,3 +2,10 @@
 (setq visible-bell nil)
 (setq current-language-environment "UTF-8")
 (setq imenu-sort-function 'imenu--sort-by-name)
+(defadvice find-file
+  (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir)))))
