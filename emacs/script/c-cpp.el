7cd128b789dc6896f6a526b9ab16d6372a76ec04
Yichao Yu <yyc1992@gmail.com>
2012-07-06 08:42:06 -0400

    [emacs] lots of highlighting, c source is a looooooot prettier now

--- emacs/script/c-cpp.el
+++ emacs/script/c-cpp.el
@@ -1,5 +1,8 @@
 (add-hook 'c-mode-hook 'my-c-mode)
 (add-hook 'c++-mode-hook 'my-c-mode)
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration t)
+(require 'font-lock)
 (defun my-c-mode()
   (interactive)
   (define-key c-mode-map (kbd "<return>") 'newline-and-indent)
@@ -18,5 +21,60 @@
   (setq c-basic-offset 4)
   (imenu-add-menubar-index)
   (which-function-mode t)
+  (font-lock-add-keywords
+   nil
+   '(
+     ("\\<\\([_0-9A-Z]+\\)[[:space:]]*(" 1 font-lock-macro-call-face)
+     ("\\<\\(\\sw+\\)[[:space:]]*(" 1 font-lock-function-call-face)
+     ;; Valid hex number (will highlight invalid suffix though)
+     ("\\b0x[[:xdigit:]]+[uUlL]*\\b" . font-lock-string-face)
+     ;; Invalid hex number
+     ("\\b0x\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
+     ;; Valid floating point number.
+     ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b"
+      (1 font-lock-string-face) (3 font-lock-string-face))
+
+     ;; Invalid floating point number.  Must be before valid decimal.
+     ("\\b[0-9].*?\\..+?\\b" . font-lock-warning-face)
+
+     ;; Valid decimal number.  Must be before octal regexes otherwise 0 and 0l
+     ;; will be highlighted as errors.  Will highlight invalid suffix though.
+     ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" 1 font-lock-string-face)
+
+     ;; Valid octal number
+     ("\\b0[0-7]+[uUlL]*\\b" . font-lock-string-face)
+
+     ;; Floating point number with no digits after the period.  This must be
+     ;; after the invalid numbers, otherwise it will "steal" some invalid
+     ;; numbers and highlight them as valid.
+     ("\\b\\([0-9]+\\)\\." (1 font-lock-string-face))
+
+     ;; Invalid number.  Must be last so it only highlights anything not
+     ;; matched above.
+     ("\\b[0-9]\\(\\w\\|\\.\\)+?\\b" . font-lock-warning-face)
+     ("\\b\\([_0-9A-Z]+\\)\\b" 1 font-lock-macro-face)
+     ("\\(?:->\\|\\.\\)[[:space:]]*\\(\\sw+\\)\\b" 1 font-lock-member-face)
+     ) t)
   )
+;; create a face for function calls
+(defface font-lock-function-call-face
+  '((t (:foreground "DarkGreen" :weight bold)))
+  "Font Lock mode face used to highlight function calls."
+  :group 'font-lock-highlighting-faces)
+(defface font-lock-macro-call-face
+  '((t (:foreground "DarkViolet" :weight bold)))
+  "Font Lock mode face used to highlight macro calls."
+  :group 'font-lock-highlighting-faces)
+(defface font-lock-macro-face
+  '((t (:foreground "DarkBlue")))
+  "Font Lock mode face used to highlight macro calls."
+  :group 'font-lock-highlighting-faces)
+(defface font-lock-member-face
+  '((t (:foreground "chocolate")))
+  "Font Lock mode face used to highlight macro calls."
+  :group 'font-lock-highlighting-faces)
+(defvar font-lock-function-call-face 'font-lock-function-call-face)
+(defvar font-lock-macro-call-face 'font-lock-macro-call-face)
+(defvar font-lock-macro-face 'font-lock-macro-face)
+(defvar font-lock-member-face 'font-lock-member-face)
 (require 'xcscope)
