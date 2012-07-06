dd4f6926f309397e446626ecceb8b020519dc8ae
Yichao Yu <yyc1992@gmail.com>
2012-07-05 20:36:44 -0400

    [emacs] -

--- emacs/script/binding.el
+++ emacs/script/binding.el
@@ -35,7 +35,7 @@
 (global-set-key (kbd "M-h") 'windmove-left)
 (global-set-key (kbd "M-k") 'windmove-up)
 (global-set-key (kbd "M-l") 'windmove-right)
-(global-set-key (kbd "<return>") 'newline-and-indent)
+;(global-set-key (kbd "<return>") 'newline-and-indent)
 (global-set-key (kbd "C-m") 'newline-and-indent)
 (global-set-key (kbd "C-c C-j") 'comment-region)
 (global-set-key (kbd "C-c C-k") 'uncomment-region)
