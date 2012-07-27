9fcd4e1746d4022432599d739ddbcc5793c1a77e
Yichao Yu <yyc1992@gmail.com>
2012-07-27 13:19:41 -0400

    [emacs] -

--- emacs/script/autoload.el
+++ emacs/script/autoload.el
@@ -1,17 +1,17 @@
 (autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
 (add-to-list 'auto-mode-alist
-             '("/\\(_\\|\\)PKGBUILD\\(-git\\|\\)$" . pkgbuild-mode))
+             '("/\\(_\\)?PKGBUILD\\(-git\\)?$" . pkgbuild-mode))
 
 (autoload 'php-mode "php-mode.el" "Php mode." t)
-(add-to-list 'auto-mode-alist '("/*.\.php[345]?$" . php-mode))
+(add-to-list 'auto-mode-alist '("\.php[345]?$" . php-mode))
 
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("/*.\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
 
 (autoload 'js2-mode "js2-mode.el" "Javascript mode." t)
-(add-to-list 'auto-mode-alist '("/*.\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("/*.\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\.json$" . js2-mode))
 
 (autoload 'vala-mode "vala-mode.el" "Major mode for editing Vala code." t)
 (add-to-list 'auto-mode-alist '("\.vala$" . vala-mode))
@@ -21,8 +21,8 @@
 (require 'mmm-mode)
 
 (autoload 'cmake-mode "cmake-mode.el" "CMake mode" t)
-(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
-(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode))
+(add-to-list 'auto-mode-alist '("CMakeLists\.txt$" . cmake-mode))
+(add-to-list 'auto-mode-alist '("\.cmake$" . cmake-mode))
 
 (require 'go-mode-load)
 
