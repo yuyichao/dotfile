0559380633520715a42c3f7ea2fa31975df672f5
Yichao Yu <yyc1992@gmail.com>
2012-10-27 12:35:35 -0400

    [zsh] ps

--- zsh/loader.zsh
+++ zsh/loader.zsh
@@ -1,4 +1,4 @@
-# Load all rc files
+# Init files loader
 
 # Load all options first
 source "$ZSH/options.zsh"
@@ -6,6 +6,7 @@ source "$ZSH/options.zsh"
 # Set environment variables
 source "$ZSH/env.zsh"
 
+# Load all rc files
 for config_file in "$ZSH/script/"*.zsh; do
     source "$config_file"
 done
@@ -14,33 +15,6 @@ return
 
 # add a function path
 fpath=("$ZSH/functions" "$ZSH/completions" "${fpath[@]}")
-
-is_plugin() {
-    local base_dir="$1"
-    local name="$2"
-    test -f "$base_dir/plugins/$name/$name.plugin.zsh" ||
-    test -f "$base_dir/plugins/$name/_$name"
-}
-
-# Add all defined plugins to fpath. This must be done
-# before running compinit.
-for plugin in "${plugins[@]}"; do
-    if is_plugin "$ZSH_CUSTOM" "$plugin"; then
-        fpath=("$ZSH_CUSTOM/plugins/$plugin" "${fpath[@]}")
-    elif is_plugin "$ZSH" "$plugin"; then
-        fpath=("$ZSH/plugins/$plugin" "${fpath[@]}")
-    fi
-done
-
 # Load and run compinit
 autoload -U compinit
 compinit -i
-
-# Load all of the plugins that were defined in ~/.zshrc
-for plugin in "${plugins[@]}"; do
-    if [[ -f "$ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-        source "$ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh"
-    elif [ -f "$ZSH/plugins/$plugin/$plugin.plugin.zsh" ]; then
-        source "$ZSH/plugins/$plugin/$plugin.plugin.zsh"
-    fi
-done
