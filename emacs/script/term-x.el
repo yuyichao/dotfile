74ffd5545b53bcecd67f59f9e8d456183e95b886
Yichao Yu <yyc1992@gmail.com>
2012-11-10 20:25:57 -0500

    [emacs] xterm-title-mode works bad with konsole....

--- emacs/script/term-x.el
+++ emacs/script/term-x.el
@@ -3,7 +3,7 @@
     (require 'xclip)
     (turn-on-xclip)
     (require 'xterm-title)
-    (xterm-title-mode t)
+    ;; (xterm-title-mode t)
     (xterm-mouse-mode t))
   (when (window-system)
     (setq x-select-enable-clipboard t))
