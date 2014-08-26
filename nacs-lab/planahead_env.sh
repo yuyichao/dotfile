4de69de077a46aa5b167093fff54299afb3b28a1
Yichao Yu <yyc1992@gmail.com>
2014-08-25 22:59:31 -0400

    [nacs-lab] rename

--- nacs-lab/planahead_env.sh
+++ nacs-lab/planahead_env.sh
@@ -1,3 +1,6 @@
-# PLANAHEAD_MAP[]=
-PLANAHEAD_INSTALL_DIR=~/.cache/nacs-lab/planahead/
+# XILINX_MAP[]=
+XILINX_MAP[hw_src]=~/projects/lab-ctrl/zynq/aluminizer/fpga/hw_src
+XILINX_MAP[customIP]=~/projects/lab-ctrl/zynq/aluminizer/fpga/customIP
+
+XILINX_INSTALL_DIR=~/.cache/nacs-lab/planahead/
 NEW_HOME=~/.config/nacs-lab/planahead_home/
