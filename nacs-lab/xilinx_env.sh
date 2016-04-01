da59772b97f5b746b72b85ccc5069b13e5c79bb3
Yichao Yu <yyc1992@gmail.com>
2016-04-01 12:29:59 -0400

    [nacs-lab] clean up

--- nacs-lab/xilinx_env.sh
+++ nacs-lab/xilinx_env.sh
@@ -1,6 +1,2 @@
-# XILINX_MAP[]=
-XILINX_MAP[hw_src]=~/projects/lab-ctrl/zynq/aluminizer/fpga/hw_src
-XILINX_MAP[customIP]=~/projects/lab-ctrl/zynq/aluminizer/fpga/customIP
-
 XILINX_INSTALL_DIR=~/.cache/nacs-lab/planahead/
 NEW_HOME=~/.config/nacs-lab/planahead_home/
