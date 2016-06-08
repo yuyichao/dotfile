602dd93f2c25db30cd47077f2c6b7b039b3c1396
Yichao Yu <yyc1992@gmail.com>
2016-06-08 18:16:09 -0400

    [emacs] try to support emacs 22

--- emacs/script/latex.el
+++ emacs/script/latex.el
@@ -1,7 +1,8 @@
-(ignore-errors
-  (progn
-    (load "auctex.el" nil t t)
-    (load "preview-latex.el" nil t t)))
+(condition-case nil
+    (progn
+      (load "auctex.el" nil t t)
+      (load "preview-latex.el" nil t t))
+  (error nil))
 (defun my-latex-mode()
   (interactive)
   (define-key LaTeX-mode-map [return] 'newline-and-indent)
