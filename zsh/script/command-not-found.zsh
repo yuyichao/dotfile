0e2fb8a138065b043113a28af1fb2ae3f2eba63d
Yichao Yu <yyc1992@gmail.com>
2017-01-23 15:01:49 -0500

    [zsh] print error message in command_not_find_handler on 5.3+ since zsh do not print it anymore

--- zsh/script/command-not-found.zsh
+++ zsh/script/command-not-found.zsh
@@ -1,15 +1,19 @@
+autoload -U is-at-least
+
 command_not_found_handler() {
     local pkgs cmd="$1"
-    if ! which pkgfile &> /dev/null; then
-        return 127
+    if which pkgfile &> /dev/null; then
+        pkgs=(${(f)"$(pkgfile -b -v -- "${cmd}" 2>/dev/null)"})
+        if [[ -n "${pkgs}" ]]; then
+            print -R "${cmd} may be found in the following packages:"
+            for pkg in "${pkgs[@]}"; do
+                print -R "  ${pkg}"
+            done
+            return 0
+        fi
     fi
-    pkgs=(${(f)"$(pkgfile -b -v -- "${cmd}" 2>/dev/null)"})
-    if [[ -n "${pkgs}" ]]; then
-        print -R "${cmd} may be found in the following packages:"
-        for pkg in "${pkgs[@]}"; do
-            print -R "  ${pkg}"
-        done
-        return 0
+    if is-at-least 5.3; then
+        echo "zsh: command not found: ${cmd}"
     fi
     return 127
 }
