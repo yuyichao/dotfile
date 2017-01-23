1151bd8e64d43a8d022193265687585063bed45e
Yichao Yu <yyc1992@gmail.com>
2017-01-23 13:28:10 -0500

    [zsh] handle the case where pkgfile is not installed

--- zsh/script/command-not-found.zsh
+++ zsh/script/command-not-found.zsh
@@ -1,6 +1,8 @@
 command_not_found_handler() {
     local pkgs cmd="$1"
-
+    if ! which pkgfile &> /dev/null; then
+        return 127
+    fi
     pkgs=(${(f)"$(pkgfile -b -v -- "${cmd}" 2>/dev/null)"})
     if [[ -n "${pkgs}" ]]; then
         print -R "${cmd} may be found in the following packages:"
