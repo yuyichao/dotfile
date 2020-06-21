a3c1b2902b8ea93fbb7762c6ba91c1457e9d32f3
Yichao Yu <yyc1992@gmail.com>
2020-06-21 13:29:33 -0400

    [git] separate windows and linux settingns

--- git/install.sh
+++ git/install.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-if test -f gitconfig-$profile; then
-    link_home gitconfig-$profile .gitconfig
+if ((islinux)); then
+    if test -f gitconfig-$profile; then
+        link_home gitconfig-$profile .gitconfig
+    else
+        link_home gitconfig .gitconfig
+    fi
 else
-    link_home gitconfig .gitconfig
+    if test -f gitconfig-$profile.win; then
+        link_home gitconfig-$profile.win .gitconfig
+    elif test -f gitconfig-$profile; then
+        link_home gitconfig-$profile .gitconfig
+    else
+        link_home gitconfig .gitconfig
+    fi
 fi
+
 if test -z "$XDG_CONFIG_HOME"; then
-   link_home gitignore .config/git/ignore
+    link_home gitignore .config/git/ignore
 else
-   link_abs gitignore "${XDG_CONFIG_HOME}/git/ignore"
+    link_abs gitignore "${XDG_CONFIG_HOME}/git/ignore"
 fi
