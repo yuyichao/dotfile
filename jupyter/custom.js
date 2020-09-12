c30976cee965c34ee20cd8a3277b95d430535e4b
Yichao Yu <yyc1992@gmail.com>
2020-09-12 18:05:49 -0400

    [jupyter] try to disable multiple cursor

--- jupyter/custom.js
+++ jupyter/custom.js
@@ -1,1 +1,17 @@
-IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+// IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+
+function _apply_cm_options(config) {
+    config.configureMouse = (cm, repeat, ev) => ({ addNew : false });
+}
+
+define([
+    'base/js/namespace',
+], function(Jupyter, promises) {
+    _apply_cm_options(Jupyter.CodeCell.options_default.cm_config);
+    // Somehow there could be existing cells when this runs...
+    Jupyter.notebook.get_cells().map(
+        function(c) {
+            _apply_cm_options(c.code_mirror.options);
+        }
+    );
+});
