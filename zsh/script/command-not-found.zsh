9b3675034ab05dc72e5d222478efd7f8187f6c5a
Yichao Yu <yyc1992@gmail.com>
2020-09-15 09:42:56 -0400

    [zsh] command not found hook using pacman -F

--- zsh/script/command-not-found.zsh
+++ zsh/script/command-not-found.zsh
@@ -11,6 +11,15 @@ command_not_found_handler() {
             done
             return 0
         fi
+    elif which pacman &> /dev/null; then
+        pkgs=(${(f)"$(pacman -F -- "${cmd}" 2>/dev/null)"})
+        if [[ -n "${pkgs}" ]]; then
+            print -R "${cmd} may be found in the following packages:"
+            for pkg in "${pkgs[@]}"; do
+                print -R "  ${pkg}"
+            done
+            return 0
+        fi
     fi
     if is-at-least 5.3; then
         echo "zsh: command not found: ${cmd}"
