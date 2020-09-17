9f95e5360305361b5f0397dad4b7d76eab37f1e1
Yichao Yu <yyc1992@gmail.com>
2020-09-17 02:20:54 -0400

    [emacs] quote auto update

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -5,60 +5,50 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-command-style (quote (("" "%(PDF)%(latex) --shell-escape %S%(PDFout)"))))
+ '(LaTeX-command-style '(("" "%(PDF)%(latex) --shell-escape %S%(PDFout)")))
  '(TeX-PDF-mode t)
  '(TeX-debug-bad-boxes t)
  '(TeX-debug-warnings t)
  '(auto-save-list-file-prefix "~/.emacs.d/auto-save-list/saves-")
- '(c-doc-comment-style (quote ((java-mode . javadoc) (pike-mode . autodoc))))
- '(c-label-minimum-indentation (quote set-from-style))
+ '(c-doc-comment-style '((java-mode . javadoc) (pike-mode . autodoc)))
+ '(c-label-minimum-indentation 'set-from-style)
  '(column-number-mode t)
  '(global-whitespace-mode t)
  '(history-length t)
  '(julia-indent-offset 4)
  '(octave-block-offset 4)
- '(pdb-path (quote /usr/lib/python3\.3/pdb\.py))
+ '(pdb-path '/usr/lib/python3\.3/pdb\.py)
  '(pkgbuild-update-sums-on-save nil)
  '(py-beep-if-tab-change nil)
- '(py-complete-function (quote py-shell-complete))
+ '(py-complete-function 'py-shell-complete)
  '(py-hide-show-minor-mode-p t)
  '(py-shebang-startstring "#!/usr/bin/env python")
  '(py-start-run-ipython-shell nil)
  '(py-start-run-py-shell nil)
  '(py-use-number-face-p nil t)
  '(safe-local-variable-values
-   (quote
-    ((eval c-set-offset
-           (quote arglist-close)
-           0)
-     (eval c-set-offset
-           (quote arglist-intro)
-           (quote ++))
-     (eval c-set-offset
-           (quote case-label)
-           0)
-     (eval c-set-offset
-           (quote statement-case-open)
-           0)
-     (eval c-set-offset
-           (quote substatement-open)
-           0)
+   '((TeX-master . dissertation\.tex)
+     (LaTeX-command-style
+      ("" "SOURCE_DATE_EPOCH=0 %(PDF)%(latex) --shell-escape %S%(PDFout)"))
+     (LaTeX-command-style quote
+                          (("" "SOURCE_DATE_EPOCH=0 %(PDF)%(latex) --shell-escape %S%(PDFout)")))
+     (eval c-set-offset 'arglist-close 0)
+     (eval c-set-offset 'arglist-intro '++)
+     (eval c-set-offset 'case-label 0)
+     (eval c-set-offset 'statement-case-open 0)
+     (eval c-set-offset 'substatement-open 0)
      (c-file-offsets
       (innamespace . 0)
       (inline-open . 0)
       (case-label . +))
      (eval progn
-           (c-set-offset
-            (quote innamespace)
-            (quote 0))
-           (c-set-offset
-            (quote inline-open)
-            (quote 0)))
+           (c-set-offset 'innamespace '0)
+           (c-set-offset 'inline-open '0))
      (js2-basic-offset . 4)
      (js-indent-level . 4)
      (sh-basic-offset . 4)
      (sh-indent-comment . t)
-     (py-indent-offset . 4))))
+     (py-indent-offset . 4)))
  '(savehist-file "~/.emacs.d/savehist")
  '(savehist-mode t)
  '(sh-shell-file "/bin/bash")
@@ -67,13 +57,12 @@
  '(tool-bar-mode nil)
  '(vc-follow-symlinks t)
  '(verilog-align-ifelse t)
- '(verilog-auto-lineup (quote ignore))
+ '(verilog-auto-lineup 'ignore)
  '(verilog-auto-newline nil)
  '(verilog-minimum-comment-distance 0)
  '(which-function-mode t)
  '(whitespace-style
-   (quote
-    (face tabs spaces trailing space-before-tab indentation empty space-after-tab space-mark tab-mark))))
+   '(face tabs spaces trailing space-before-tab indentation empty space-after-tab space-mark tab-mark)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
