c70c89e5c78787a64cd6eefad1ffb2743c8a60d5
Yichao Yu <yyc1992@gmail.com>
2026-09-03 15:43:17 -0400

    keyboard management update

--- emacs/script/term-x.el
+++ emacs/script/term-x.el
@@ -10,10 +10,10 @@
     (xterm-mouse-mode t))
   (when (window-system)
     (setq x-select-enable-clipboard t)
-    (set-frame-parameter (selected-frame) 'alpha '(80 80))
-    (shell-command
-     (concat "xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c "
-             "-set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id "
-             (frame-parameter (selected-frame) 'outer-window-id)))
-    (add-to-list 'default-frame-alist '(alpha 80 80)))
+    (set-frame-parameter (selected-frame) 'alpha-background 90)
+    ;; (shell-command
+    ;;  (concat "xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c "
+    ;;          "-set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id "
+    ;;          (frame-parameter (selected-frame) 'outer-window-id)))
+    (add-to-list 'default-frame-alist '(alpha-background 90)))
   )
